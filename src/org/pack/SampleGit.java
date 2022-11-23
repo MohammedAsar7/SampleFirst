44b94a3d56b15938d96804cd32db79568fea174e
MohammedAsar7 <mohammedasar017@gmail.com>
2022-11-23 10:15:02 +0530

    Few lines of code added

---
+++ src/org/pack/SampleGit.java
@@ -0,0 +1,15 @@
+package org.pack;
+
+public class SampleGit {
+	int a = 100;
+	int b = 100;
+
+	private void b() {
+		System.out.println("b method");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("git hub");
+	}
+
+}
